62334c14afff8971ac122f9e6cf7ff7e7b5abb9d
Sean Fahey <sean.fahey@dexcom.com>
2025-09-08 10:01:10 -0700

    improved instructions

--- interview_project_JAVA/src/Main.java
+++ interview_project_JAVA/src/Main.java
@@ -8,12 +8,14 @@ import java.util.*;
  * This is the entry point for the project. Edit the `main` method to implement the following:
  *
  * 1) Get the list of connected Android devices.
+ * 
  * 2) For each device do the following:
  *    2a) Find and click the `PROFILE` button.
  *    2b) Check if the `APP_VERSION` button is present.
- *    2c) If the `APP_VERSION` button is present, return the text value of the element.
- *    2d) If the `APP_VERSION` button is not present, return null.
- * 3) Print the app version for each device.
+ *      2b.1) If the `APP_VERSION` button is present, get the text value of the element.
+ *      2b.2) If the `APP_VERSION` button is not present, get the value `null`.
+ * 
+ * 3) Print the app version or null for each device.
  */
 public class Main {
     public static void main(String[] args) {
@@ -27,5 +29,6 @@ public class Main {
         String showUsernameLocatorValue = "show_username_element";
 
         System.out.println("Hello World!");
+        System.out.println(profileLocatorType + '\t' + profileLocatorValue);
     }
 }
