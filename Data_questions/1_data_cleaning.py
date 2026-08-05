a86a77657ed508230cc668776b53ab771e331597
Sean Fahey <sean.fahey@dexcom.com>
2026-08-05 23:15:32 +0000

    Switched to occupation instead of stress level float

--- Data_questions/1_data_cleaning.py
+++ Data_questions/1_data_cleaning.py
@@ -3,13 +3,13 @@
 I've provided a function to read a csv file into pandas. There are known data
 quality issues with the file. Do the following:
 
-1. Tell me the average Sleep_Hours for each Stress_Level.
-2. Remove any rows that are exact duplicates.
+1. Tell me the average Sleep_Hours for each Occupation.
+2. Remove any rows that are exact duplicates using df.drop_duplicates().
 3. Print how many columns have missing values.
 4. Delete any rows with missing values.
 5. Identify any columns that contain impossible values.
 6. Delete any rows that contain impossible values.
-7. Tell me the new average Sleep_Hours for each Stress_Level.
+7. Tell me the new average Sleep_Hours for each Occupation.
 
 """
 
@@ -24,5 +24,8 @@ def load_data(file_path='Data_questions/data/smartphone_usage_sample_messy.csv')
     return data
 
 df = load_data()
+
 print(df.head())
 print(df.info())
+
+print(f'Total app usage count of Freelancers: {df[df["Occupation"] == "Freelancer"]["App_Usage_Count"].sum()}')
